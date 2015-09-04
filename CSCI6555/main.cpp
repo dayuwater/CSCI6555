2d1c8c7a3fea2711e6c538e860db494edd504c8b
Tan Wang <tanwang@Tans-MacBook-Pro.local>
2015-09-03 20:02:54 -0400

    Initial Commit

---
+++ CSCI6555/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSCI6555
+//
+//  Created by Tan Wang on 9/3/15.
+//  Copyright (c) 2015 Tan Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
